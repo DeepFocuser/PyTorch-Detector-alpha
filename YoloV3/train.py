38f21f702a546b4624461dc73afddc6111bea03c
DeepFocuser <58889565+DeepFocuser@users.noreply.github.com>
2020-09-05 03:35:37 +0900

    Update train.py

--- YoloV3/train.py
+++ YoloV3/train.py
@@ -178,7 +178,10 @@ def run(mean=[0.485, 0.456, 0.406],
                  alloc_size=offset_alloc_size)
 
     # https://github.com/sksq96/pytorch-summary / because of anchor, not working
-    # modelsummary(net.to(context), input_shape[1:])
+    #if GPU_COUNT == 0:
+    #    modelsummary(net.to(context), input_shape[1:], device="cpu")
+    #elif GPU_COUNT == 1:
+    #    modelsummary(net.to(context), input_shape[1:], device="cuda")
 
     if tensorboard:
         summary = SummaryWriter(log_dir=os.path.join("torchboard", model), max_queue=10, flush_secs=10)
