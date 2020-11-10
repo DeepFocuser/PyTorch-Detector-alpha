71455a20dcc9947a9457a42997916252ee3dbd8a
DeepFocuser <58889565+DeepFocuser@users.noreply.github.com>
2020-11-10 09:15:04 +0900

    Update transformer.py

--- CenterNet/core/utils/dataprocessing/transformer.py
+++ CenterNet/core/utils/dataprocessing/transformer.py
@@ -92,10 +92,10 @@ class CenterTrainTransform(object):
             bbox = box_resize(bbox, (w, h), (output_w, output_h))
 
         # heatmap 기반이기 때문에 제한 해줘야 한다.
-        bbox[:, 0] = np.clip(bbox[:, 0], 0, output_w)
-        bbox[:, 1] = np.clip(bbox[:, 1], 0, output_h)
-        bbox[:, 2] = np.clip(bbox[:, 2], 0, output_w)
-        bbox[:, 3] = np.clip(bbox[:, 3], 0, output_h)
+        bbox[:, 0] = np.clip(bbox[:, 0], 0, output_w-1)
+        bbox[:, 1] = np.clip(bbox[:, 1], 0, output_h-1)
+        bbox[:, 2] = np.clip(bbox[:, 2], 0, output_w-1)
+        bbox[:, 3] = np.clip(bbox[:, 3], 0, output_h-1)
 
         img = self._toTensor(img)  # 0 ~ 1 로 바꾸기
         img = torch.sub(img, self._mean)
@@ -143,10 +143,10 @@ class CenterValidTransform(object):
         img = torch.div(img, self._std)
 
         # heatmap 기반이기 때문에 제한 해줘야 한다.
-        bbox[:, 0] = np.clip(bbox[:, 0], 0, output_w)
-        bbox[:, 1] = np.clip(bbox[:, 1], 0, output_h)
-        bbox[:, 2] = np.clip(bbox[:, 2], 0, output_w)
-        bbox[:, 3] = np.clip(bbox[:, 3], 0, output_h)
+        bbox[:, 0] = np.clip(bbox[:, 0], 0, output_w-1)
+        bbox[:, 1] = np.clip(bbox[:, 1], 0, output_h-1)
+        bbox[:, 2] = np.clip(bbox[:, 2], 0, output_w-1)
+        bbox[:, 3] = np.clip(bbox[:, 3], 0, output_h-1)
 
         if self._make_target:
             bbox = bbox[np.newaxis, :, :]
