291250c8f3c1bc445d8a415b8dd36f9aaae1a86f
DeepFocuser <58889565+DeepFocuser@users.noreply.github.com>
2021-03-23 21:07:17 +0900

    Update prediction.py

--- CenterNet/core/utils/dataprocessing/prediction.py
+++ CenterNet/core/utils/dataprocessing/prediction.py
@@ -107,11 +107,15 @@ class Prediction(nn.Module):
         offset = offset.permute(0, 2, 3, 1).reshape(
             (batch, -1, 2))  # (batch, x, y, channel) -> (batch, height*width, 2)
         wh = wh.permute(0, 2, 3, 1).reshape((batch, -1, 2))  # (batch, width, height, channel) -> (batch, height*width, 2)
-        topk_indices = torch.fmod(indices, float((height * width))) # 클래스별 index, why float? For compatibility of torchscript and pytorch 1.7.0 /output dtype : indices.dtype
-
+        
+        # 클래스별 index, why float? For compatibility of torchscript and pytorch 1.7.0 /output dtype : indices.dtype
+        # 클래스별 index, why float? For compatibility of torchscript and pytorch 1.8.0 /output dtype : float32
+        topk_indices = torch.fmod(indices, float((height * width)))
+        topk_indices = topk_indices.to(indices.dtype)
+        
         # 2차원 복구
         topk_ys = torch.floor_divide(topk_indices, width)  # y축 index
-        topk_xs = torch.fmod(topk_indices, float(width))  # x축 index, why float? For compatibility of torchscript and pytorch 1.7.0 /output dtype : indices.dtype
+        topk_xs = torch.fmod(topk_indices, float(width))  # x축 index
 
         batch_indices = torch.arange(batch, device=ids.device).unsqueeze(dim=-1)
         batch_indices = batch_indices.repeat_interleave(self._topk, dim=-1) # (batch, self._topk)
